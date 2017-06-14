4313f6df49bc3cbd747aec0a43537752e21ab9ff
Adien7638 <avianshverma8922003966@gmail.com>
2017-06-14 16:17:18 +0530

    Web chat

--- public/main.js
+++ public/main.js
@@ -1,13 +1,41 @@
-
+var video;
+var socket = io();
+var vScale = 8;
 function setup(){
+  createCanvas(600,400);
+  background(51);
 
+  video = createCapture(VIDEO);
+  video.size(width/vScale,height/vScale);
+  pixelDensity(1);
+  socket.on('broadcast',function(data){
 
+  for (var y = 0; y<video.height ; ++y){
+		for(var x = 0; x<video.width ; ++x){
+			var index = (x+y*video.width);
+			fill(color(data[index],data[index],data[index]));
+			rect(x*vScale,y*vScale,vScale,vScale);
+		}
+	}
+  });
 }
 
 
+
 function draw(){
-
-
+	video.loadPixels();
+	loadPixels();
+	var data  = [];
+	for (var y = 0; y<video.height ; ++y){
+		for(var x = 0; x<video.width ; ++x){
+			var index = (x+y*video.width)*4;
+			var r = video.pixels[index+0];
+			var g = video.pixels[index+1];
+			var b = video.pixels[index+2];
+			data.push((r+g+b)/3);
+		}
+	}
+	socket.emit('chatVideo',data);
 
 }
 
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var express = require('express');
+var io = require('socket.io')(http);
+app.use(express.static('public'));
 
-app.get('/',function(req,res){
-	res.sendfile('libraries');
+io.on('connection',function(socket){
+	console.log("A user : "+socket.id+" is connected");
+
+	 socket.on('chatVideo',function(data){
+	 	io.sockets.emit('broadcast',data);
+	 });
+
+	socket.on('disconnect',function(){
+		
+		console.log('User : '+socket.id+" is disconnect");
+	});
 });
+
 
 http.listen('3000',function(){
 	console.log('listening on : 3000');
